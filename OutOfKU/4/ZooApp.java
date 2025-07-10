ebc7e7c6171801b6cf3d7ce35a9ba8d606c77514
SharKPonG <Pingkiesdev@gmail.com>
2025-07-10 19:35:24 +0700

    Punyapiwut

---
+++ OutOfKU/4/ZooApp.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+// SuperClass
+class Animal {
+    private String name;
+    private int age;
+    private String species;
+    
+    
+    //constructer
+    Animal(String name, int age, String species){
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+
+
+    // method
+    public void makeSound(){
+        System.out.println("Animal sound");
+    }
+
+    public void showInfo(){
+        System.out.println("Name"+ name);
+        System.out.println("Age" + age);
+        System.out.println("Species" + species);
+    }
+    public String getSpecies(){
+        return species;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void hunt(){
+
+    }
+}
+
+//Sub class 
+
+class Carnivore extends Animal {
+
+    private boolean isHungry;
+
+
+
+    public Carnivore(String name, int age, String species, boolean isHungry){
+        super(name,age,species);
+        this.isHungry = isHungry;
+    }
+
+
+    public void hunt(){
+        if (isHungry == true){
+            System.out.println("The animal is hunting");
+        }else{
+            System.out.println("The animal is not hungry");
+        }
+
+    }
+
+    // Overide
+    public void makeSound(){
+        System.out.println("Roar!");
+    }
+
+    public void showInfo(){
+        super.showInfo();
+        System.out.println("Is hungry" + isHungry);
+    }
+}
+
+
+public class ZooApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Carnivore[] carni = new Carnivore[3];
+
+        for (int i = 0; i < carni.length; i++) {
+            
+            String name = in.nextLine();
+
+            int age = Integer.parseInt(in.nextLine());
+
+            String species = in.nextLine();
+
+            boolean isHungry = Boolean.parseBoolean(in.nextLine());
+
+
+            carni[i] = new Carnivore(name, age, species, isHungry);
+
+            if (age < 3 ){
+                System.out.println("This is a young animal");
+            }else{
+                System.out.println("This is an adult animal");
+            }
+        }
+
+
+        System.out.println("=== Animal Information ===");
+        for (Carnivore car : carni){
+            car.showInfo();
+            car.hunt();
+        }
+
+
+        in.close();
+    }
+}
